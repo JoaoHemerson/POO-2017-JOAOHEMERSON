9d98ba01ef765f119cde12f0f618625c0e8a4b5a
Joao Hemerson <joaohemerson@gmail.com>
2017-12-13 23:04:02 -0300

    corrigindo ultimo arquivo enviado

--- cap07/src/app/TestaConta.java
+++ cap07/src/app/TestaConta.java
@@ -20,21 +20,34 @@ public class TestaConta {
 			
 //			cc.atualiza(0.01);
 //			cp.atualiza(0.01);
+			contascorrentes[0].atualiza(0.01);
+			contaspoupanca[0].atualiza(0.01);
 			
 //			System.out.println(c.getSaldo());
 //			System.out.println(cc.getSaldo());
 //			System.out.println(cp.getSaldo());
+			
+
+			
+			System.out.println(contascorrentes[0].getSaldo());
+			System.out.println(contaspoupanca[0].getSaldo());
 			
 			AtualizadorDeConta adc = new AtualizadorDeConta(0.01);
 			
 //			adc.roda(c);
 //			adc.roda(cc);
 //			adc.roda(cp);
+			adc.roda(contascorrentes[0]);
+			adc.roda(contaspoupanca[0]);
 			
 //			System.out.println("Saldos Atualizados");
 //			System.out.println(c.getSaldo());
 //			System.out.println(cc.getSaldo());
 //			System.out.println(cp.getSaldo());
+			System.out.println("Saldos Atualizados");
+			
+			System.out.println(contascorrentes[0].getSaldo());
+			System.out.println(contaspoupanca[0].getSaldo());
 			System.out.println("***************************");
 			System.out.println("Saldo Total " + adc.getSaldoTotal());
 									
